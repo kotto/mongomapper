eb1709af4979b15fced1ea786909dd404cdc156d
Cyril Mougel <cyril.mougel@af83.com>
2009-10-20 22:46:54 +0200

    update mocha gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.join(File.expand_path(File.dirname(__FILE__) + '/../lib/mongo_mappe
 gem 'jnunemaker-matchy', '0.4.0'
 gem 'shoulda', '2.10.2'
 gem 'timecop', '0.3.1'
-gem 'mocha', '0.9.4'
+gem 'mocha', '0.9.8'
 
 require 'matchy'
 require 'shoulda'
